4a9be29c0ed69dc10edda84d17080cf905548413
Tucker Hoog <tuckerhoog@tutanota.com>
2021-08-16 20:35:55 -0400

    test pass...

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App component', () => {
+  
   afterEach(() => cleanup());
+  
   it('renders App', () => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
   });
+
+  it('behavioral test', () => {
+    
+    render (<App />);
+    
+    const display = screen.getByTestId('divi');
+
+    const input = screen.getByTestId('in');
+
+    fireEvent.change(input, { target: { value: '#3083DC' } });
+    fireEvent.change(input, { target: { value: '#7DDE92' } });
+    expect(display).toHaveStyle({ backgroundColor: '#7DDE92' });
+
+    const undo = screen.getByText('undo');
+    fireEvent.click(undo);
+    expect(display).toHaveStyle({ backgroundColor: '#3083DC' });
+
+    const redo = screen.getByText('redo');
+    fireEvent.click(redo);
+    expect(display).toHaveStyle({ backgroundColor: '#7DDE92' });
+  });
+
 });
